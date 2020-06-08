a1aee58facdab3e907e3e7a672986e6e3a1c280e
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-06-08 15:26:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,8 @@ def assign_rooms(attendees)
   end
 end
 
-def printer(attendees)
-  batch_badge_creator(badge).each do |name|
+def printer(name)
+  batch_badge_creator(badges).each do |name|
     puts name
   end
 end
